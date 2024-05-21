b31fdc7c420cea97c4a4fa47234833bd677ac9c6
Muqaddas Shoaib Ahmed <dearmuqaddas@gmail.com>
2024-05-21 10:18:19 +0500

    Fruits assignment

---
+++ arrays.ts
@@ -0,0 +1,53 @@
+// QUESTION 1
+// const fruitsName=["apple","mango","banana","orange"];
+// console.log(fruitsName)
+// QUESTION 2
+// let numberVariable:number[]=[10,20,30,40]
+// console.log(numberVariable)
+// QUESTION 3
+// let fruitsName:string[]=["apple","mango","banana","orange"];
+// console.log(fruitsName, "before")
+// fruitsName[2]="thirdFruit"
+// console.log(fruitsName,"after")
+// QUESTION 4
+// let numberVariable:number[]=[10,20,30,40];
+//  console.log(numberVariable, "before")
+//  numberVariable[1]=25
+//  console.log(numberVariable,"after")
+// QUESTION 5
+// let fruitsName:string[]=["apple","mango","banana","orange"]
+// fruitsName.push("grapes")
+// console.log(fruitsName)
+// QUESTION 6
+// let fruitsName:string[]=["apple","mango","banana","orange"]
+// fruitsName.pop()
+// fruitsName[3]="lastFruit"
+// console.log(fruitsName)
+// QUESTION 7
+// let fruitsName:string[]=["apple","mango","banana","orange"]
+// fruitsName.shift()
+// fruitsName[0]="firstFruit"
+// console.log(fruitsName)
+// QUESTION 8
+// let fruitsName:string[]=["apple","mango","banana","orange"]
+// fruitsName.unshift("kiwi")
+// console.log(fruitsName)
+// SLICING Method:   The slice method is used to extract a section of an array and returns it as a new array.
+// QUESTION 9
+// let fruitsName:string[]=["apple","mango","banana","orange"];
+// fruitsName.splice(1,2);
+// console.log(fruitsName)
+// QUESTION 10     if add in between the arrays
+// let fruitsName:string[]=["apple","mango","banana","orange"];
+// let index=2;
+// let newFruits=["pineapple","pear"]
+// fruitsName.splice(index, 0, ...newFruits);
+// console.log(fruitsName);
+// QUESTION 11
+// let fruitsName:string[]=["apple","mango","banana","orange"];
+// let citrusFruits=fruitsName.slice(0,2)
+// console.log(citrusFruits);
+// QUESTION 12
+// var fruitsName = ["apple", "mango", "banana", "orange"];
+// var lastTwoFruits = fruitsName.slice(-2);
+// console.log(lastTwoFruits);
